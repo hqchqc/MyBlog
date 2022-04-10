594bdc52a91bf302ec29e68a6499ea85494665f1
hqchqc <734986933@qq.com>
2022-04-10 23:48:26 +0800

    feat: add es6

---
+++ docs/code/01_ES6入门教程/01_let和const命令.js
@@ -0,0 +1,38 @@
+var a = [];
+for (var i = 0; i < 10; i++) {
+  a[i] = function () {
+    console.log(i);
+  };
+}
+a[6]();
+
+var a = [];
+for (let i = 0; i < 10; i++) {
+  a[i] = function () {
+    console.log(i);
+  };
+}
+a[6]();
+
+for (var i = 0; i < 10; i++) {
+  var i = "abc";
+  console.log(i + i);
+}
+{
+  console.log(foo);
+  var foo = 123;
+
+  // console.log(foo2);
+  // let foo2 = 12300;
+}
+
+if (true) {
+  foo = 4; //start
+  console.log(foo);
+
+  let foo = 3; // TDZ end
+  console.log(foo);
+
+  foo = 333;
+  console.log(foo);
+}
