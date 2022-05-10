e4e7ca23e37a9c192c5190ee8f372059be4420dc
hqchqc <734986933@qq.com>
2022-05-10 19:31:16 +0800

    :new: 新增this绑定规则细节章节

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -111,6 +111,7 @@ export default defineUserConfig<DefaultThemeOptions>({
             "/javaScript/深入JavaScript高级语法/02_函数执行过程和作用域链.md",
             "/javaScript/深入JavaScript高级语法/03_闭包的定义和内存模型.md",
             "/javaScript/深入JavaScript高级语法/04_内存回收和this绑定规则.md",
+            "/javaScript/深入JavaScript高级语法/05_this绑定规则细节及巩固.md",
           ],
         },
       ],
