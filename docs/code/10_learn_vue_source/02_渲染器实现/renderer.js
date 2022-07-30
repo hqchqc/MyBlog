d7ffa828459453eadc900d5e50fa986da892f7af
hqchqc <734986933@qq.com>
2022-07-30 20:52:36 +0800

    feat: vue

--- docs/code/10_learn_vue_source/02_渲染器实现/renderer.js
+++ docs/code/10_learn_vue_source/02_渲染器实现/renderer.js
@@ -39,3 +39,69 @@ const mount = (vnode, container) => {
   // 4. 将el挂在到container上
   container.appendChild(el);
 };
+
+const patch = (n1, n2) => {
+  if (n1.tag !== n2.tag) {
+    const n1ElParent = n1.el.parentElement;
+    n1ElParent.removeChild(n1.el);
+    mount(n2, n1ElParent);
+  } else {
+    // 1. 去除element对象，并且在n2中进行保存
+    const el = (n2.el = n1.el);
+
+    // 2. 处理props
+    const oldProps = n1.props || {};
+    const newProps = n2.props || {};
+    // 2.1 获取所有的newProps添加到el
+    for (const key in newProps) {
+      const oldValue = oldProps[key];
+      const newValue = newProps[key];
+
+      if (oldValue !== newValue) {
+        if (key.startsWith("on")) {
+          el.addEventListener(key.slice(2).toLowerCase(), newValue);
+        } else {
+          el.setAttribute(key, newValue);
+        }
+      }
+    }
+
+    // 2.2 删除旧的props
+    for (const key in oldProps) {
+      if (!(key in newProps)) {
+        if (key.startsWith("on")) {
+          const value = oldProps[key];
+          el.removeEventListener(key.slice(2).toLowerCase(), value);
+        } else {
+          el.removeAttribute(key);
+        }
+      }
+    }
+
+    // 3. 处理children
+    const oldChildren = n1.children || [];
+    const newChildren = n2.children || [];
+
+    if (typeof newChildren === "string") {
+      if (typeof oldChildren === "string") {
+        if (newChildren !== oldChildren) {
+          el.textContent = newChildren;
+        }
+      } else {
+        el.innerHTML = newChildren;
+      }
+    } else {
+      if (typeof oldChildren === "string") {
+        el.innerHTML = "";
+        newChildren.forEach((vnode) => {
+          mount(vnode, el);
+        });
+      } else {
+        // 两个都是数组
+        const commonLength = Math.min(oldChildren.length, newChildren.length);
+        if (commonLength < oldChildren.length) {
+        }
+      }
+    }
+  }
+};
